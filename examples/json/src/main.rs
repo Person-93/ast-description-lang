b3204f6eaa24f9c435ef5c2dd9454f36ced027b9
person93 <person93.person93@gmail.com>
2022-02-26 07:45:21 -0500

    test: fix up json example

--- examples/json/src/main.rs
+++ examples/json/src/main.rs
@@ -1,15 +1,32 @@
 use crate::tokens::Token;
-use chumsky::{Parser, Stream};
+use anyhow::Result;
+use chumsky::{error::Simple, Parser, Stream};
 use logos::Logos;
+use std::fmt::{self, Display, Formatter};
 
 mod ast;
 mod tokens;
 
-fn main() {
-  let json = std::fs::read_to_string("sample.json").unwrap();
+fn main() -> Result<()> {
+  let json = std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/sample.json"))?;
   let iter = Token::lexer(&json).spanned();
   let parsed = ast::parse::json()
     .parse(Stream::from_iter(json.len()..json.len() + 1, iter))
-    .unwrap();
-  println!("{parsed:#?}")
+    .map_err(Error)?;
+  println!("{parsed:#?}");
+  Ok(())
 }
+
+#[derive(Clone, Debug)]
+struct Error(Vec<Simple<Token>>);
+
+impl Display for Error {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    for (idx, err) in self.0.iter().enumerate() {
+      write!(f, "{idx}: {err}")?;
+    }
+    Ok(())
+  }
+}
+
+impl std::error::Error for Error {}
