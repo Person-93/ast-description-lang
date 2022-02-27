254161d2b3e0bcd7b4e7ea7a0f1f6b4b03d73b0a
person93 <person93.person93@gmail.com>
2022-02-27 16:04:09 -0500

    test: check output of e2e tests

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,37 +1,34 @@
-use anyhow::{anyhow, Context, Result};
-use std::{env, ffi::OsString, fs, process::Command};
+use anyhow::{ensure, Context, Result};
+use escargot::CargoBuild;
+use insta::{assert_snapshot, with_settings};
+use std::{env, fs, process::Stdio};
 
 fn main() -> Result<()> {
-  let all_ok = fs::read_dir("./examples")?
-    .filter_map(|e| {
-      let e = e.unwrap();
-      e.metadata()
-        .unwrap()
-        .file_type()
-        .is_dir()
-        .then(|| e.path().join("Cargo.toml"))
-    })
-    .map(|path| {
-      println!("running example at {}", path.display());
-      Command::new(cargo())
-        .arg("run")
-        .arg("--manifest-path")
-        .arg(path)
-        .status()
-        .context("failed to start cargo")
-        .map(|r| r.success())
-    })
-    .try_fold(true, |accum, current| {
-      current.map(|current| current && accum)
-    })?;
-
-  if all_ok {
-    Ok(())
-  } else {
-    Err(anyhow!("some examples failed"))
+  for entry in fs::read_dir("./examples").context("failed to read examples directory")? {
+    let entry = entry.context("failed to read directory entry")?;
+    if !entry.metadata()?.is_dir() {
+      continue;
+    }
+    let path = entry.path();
+    println!("running example at {}", path.display());
+    let output = CargoBuild::new()
+      .manifest_path(path.join("Cargo.toml"))
+      .run()?
+      .command()
+      .stdout(Stdio::piped())
+      .spawn()
+      .context("failed to spawn process for example")?
+      .wait_with_output()
+      .context("failed waiting for output from example")?;
+    ensure!(
+      output.status.success(),
+      "example-{} exited non zero",
+      path.file_name().unwrap().to_str().unwrap()
+    );
+    with_settings!({input_file => Some(path)}, {
+      assert_snapshot!(std::str::from_utf8(&output.stdout).unwrap())
+    });
   }
-}
 
-fn cargo() -> OsString {
-  env::var_os("CARGO").unwrap_or_else(|| OsString::from("cargo"))
+  Ok(())
 }
